954e8d585c0957e000367c9b466355d75840d369
HarshaNaik <h976431@gmail.com>
2023-12-31 00:24:15 +0530

    made some changes in AdditionOfTwoNumbers and created new program to convert Temperature to Fahrenheit

--- AdditionOfTwoNumbers.java
+++ AdditionOfTwoNumbers.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class AdditionOfTwoNumbers {
     public static void main(String[] args) {
-        System.out.println("Enter two numbers : ");
-        Scanner input = new Scanner(System.in);
-        int num1 = input.nextInt();
-        float num2 = input.nextInt();
-        float sum = num1 + num2;
-        System.out.println("The sum of two numbers is : "+ sum);
+        // System.out.println("Enter two numbers : ");
+        // Scanner input = new Scanner(System.in);
+        // int num1 = input.nextInt();
+        // float num2 = input.nextInt();
+        // float sum = num1 + num2;
+        // System.out.println("The sum of two numbers is : "+ sum);
+        float a = 20.32f;
+        int b = (int)a;
+        System.out.println(b);
     }
 }
