c9b13224e8123f42c82813351fe2c44e32ec0042
HarshaNaik <h976431@gmail.com>
2024-01-11 08:05:50 +0530

    Created new program Area which contains program to find area of different dimensions

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a= sc.nextInt();
+        // int a= sc.nextInt();
+        // System.out.println(a);
+        byte b = 2;
+        System.out.println(b);
+        int a = (int)b;
         System.out.println(a);
-        
-
     }
 }
